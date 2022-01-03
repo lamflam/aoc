0faa2936f04784c50e25bf2b68b083cee0142f16
Kevin LaFlamme <kevinl@hey.com>
2022-01-03 09:41:25 -0500

    Day 7, 2021

--- 2021/day7/index.ts
+++ 2021/day7/index.ts
@@ -1,81 +1,43 @@
 import path from 'path';
 import fs from 'fs';
 
-type Grid = number[][];
-
-function incGrid(grid: Grid, x: number, y: number): void {
-    grid[x] = grid[x] || [];
-    grid[x][y] = (grid[x][y] || 0) + 1;
-}
-
 export function part1(fileName = 'input.txt') {
-    const input = fs
+    const positions = fs
         .readFileSync(path.join(__dirname, fileName), 'utf8')
-        .split('\n')
-        .map((n) => n.trim())
-        .filter((l) => l);
-    const lines = input.map((l) => l.split(' -> ').map((p) => p.split(',').map((n) => parseInt(n))));
-    const grid: Grid = [];
+        .split(',')
+        .filter((l) => l)
+        .map((n) => parseInt(n));
 
-    lines.forEach((line) => {
-        const [p1, p2] = line;
-        const [x1, y1] = p1;
-        const [x2, y2] = p2;
-        if (x1 === x2) {
-            let minY = Math.min(y1, y2);
-            let maxY = Math.max(y1, y2);
-            for (let y = minY; y <= maxY; y++) {
-                incGrid(grid, x1, y);
-            }
-        } else if (y1 === y2) {
-            let minX = Math.min(x1, x2);
-            let maxX = Math.max(x1, x2);
-            for (let x = minX; x <= maxX; x++) {
-                incGrid(grid, x, y1);
-            }
+    let minCost = Number.MAX_SAFE_INTEGER;
+    positions.forEach((n, i) => {
+        const cost = positions.reduce((sum, m, j) => {
+            return sum + (i === j ? 0 : Math.abs(n - m));
+        }, 0);
+        if (cost < minCost) {
+            minCost = cost;
         }
     });
-
-    return grid.reduce((sum, row) => sum + row.reduce((rowSum, col) => rowSum + (col > 1 ? 1 : 0), 0), 0);
+    return minCost;
 }
 
 export function part2(fileName = 'input.txt') {
-    const input = fs
+    const positions = fs
         .readFileSync(path.join(__dirname, fileName), 'utf8')
-        .split('\n')
-        .map((n) => n.trim())
-        .filter((l) => l);
-    const lines = input.map((l) => l.split(' -> ').map((p) => p.split(',').map((n) => parseInt(n))));
-    const grid: Grid = [];
+        .split(',')
+        .filter((l) => l)
+        .map((n) => parseInt(n));
+    const max = Math.max(...positions);
 
-    lines.forEach((line) => {
-        const [p1, p2] = line;
-        const [x1, y1] = p1;
-        const [x2, y2] = p2;
-        if (x1 === x2) {
-            let minY = Math.min(y1, y2);
-            let maxY = Math.max(y1, y2);
-            for (let y = minY; y <= maxY; y++) {
-                incGrid(grid, x1, y);
-            }
-        } else if (y1 === y2) {
-            let minX = Math.min(x1, x2);
-            let maxX = Math.max(x1, x2);
-            for (let x = minX; x <= maxX; x++) {
-                incGrid(grid, x, y1);
-            }
-        } else {
-            let startX = x1 < x2 ? x1 : x2;
-            let endX = x1 < x2 ? x2 : x1;
-            let y = x1 < x2 ? y1 : y2;
-            let endY = x1 < x2 ? y2 : y1;
-            for (let x = startX; x <= endX; x++) {
-                incGrid(grid, x, y);
-                if (y <= endY) y++;
-                else y--;
-            }
+    let minCost = Number.MAX_SAFE_INTEGER;
+    for (let n = 0; n <= max; n++) {
+        const cost = positions.reduce((sum, m, j) => {
+            const dist = Math.abs(n - m);
+            const cost = (dist * (dist + 1)) / 2;
+            return sum + cost;
+        }, 0);
+        if (cost < minCost) {
+            minCost = cost;
         }
-    });
-
-    return grid.reduce((sum, row) => sum + row.reduce((rowSum, col) => rowSum + (col > 1 ? 1 : 0), 0), 0);
+    }
+    return minCost;
 }
--- 2021/day7/test.ts
+++ 2021/day7/test.ts
@@ -1,13 +1,13 @@
 import { part1, part2 } from '.';
 
-describe('day5', () => {
+describe('day7', () => {
     test('part1', () => {
-        expect(part1('testInput.txt')).toBe(5);
+        expect(part1('testInput.txt')).toBe(37);
         console.log(part1('input.txt'));
     });
 
     test('part2', () => {
-        expect(part2('testInput.txt')).toBe(12);
+        expect(part2('testInput.txt')).toBe(168);
         console.log(part2('input.txt'));
     });
 });
